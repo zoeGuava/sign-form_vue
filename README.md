e88db05d8c7bddf6e86a44a3a00066f33bd0fc2d
Kraemer <kraemer.tsai@tpisoftware.com>
2021-08-03 14:49:27 +0800

    npm init vitawind@latest {專案名稱} -- --vuecli

---
+++ README.md
@@ -0,0 +1,7 @@
+# vuecli-tailwind-jit-template
+
+- postcss: 7
+- tailwindcss: 2.2 for postcss7
+
+### To start dev server
+run `npm run serve` script.
---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <img class="mx-auto" alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
